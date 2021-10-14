be9b8e8caa221b12f2ad387ba1cf90645bf2e7f5
Anton Shlikhta <shlikhta.anton@gmail.com>
2021-10-14 18:27:06 +0300

    add github actions

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isDev = process.env.NODE_ENV !== "production";
+
 module.exports = {
   reactStrictMode: true,
-}
+  basePath: isDev ? "" : process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: isDev ? "" : process.env.NEXT_PUBLIC_BASE_PATH,
+};
