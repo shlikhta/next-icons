d501ec370607faee571e1d3d2a0a3a09e71c36a0
Anton Shlikhta <shlikhta.anton@gmail.com>
2021-11-09 11:05:39 +0200

    update icons

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { OutlineIcons } from "../src/components/OutlineIcons";
 import { SolidIcons } from "../src/components/SolidIcons";
 
 export default function Home() {
+  const [lineWidth, setLineWidth] = useState(1);
+  const [searchStr, setSearchStr] = useState('');
+
   return (
-    <div className="icons-container">
-      <OutlineIcons />
-      <SolidIcons />
+    <div className="app">
+      <div className="header">
+        <div className="search">
+          <input className='search__input' type="text" onChange={(e) => {setSearchStr(e.target.value)}} value={searchStr}/>
+        </div>
+        <div className="range">
+
+          <input className='range__input' type="range" id="stroke-width" name="stroke-width"
+                 min="0" max="3" step={0.5} value={lineWidth} onChange={(e) => {setLineWidth(e.target.value)}}/>
+          <div className='range__value'>{lineWidth}</div>
+          <style>
+            {`
+           .next-icon {
+          stroke-width: ${lineWidth};
+        }`}
+          </style>
+        </div>
+      </div>
+
+      <div className="icons-container">
+        <OutlineIcons searchStr={searchStr}/>
+        <SolidIcons searchStr={searchStr}/>
+      </div>
     </div>
   );
 }
--- src/components/IconList/index.js
+++ src/components/IconList/index.js
@@ -1,9 +1,9 @@
-export const IconList = ({ list }) => {
+export const IconList = ({ list, searchStr }) => {
   if (!list && !list.length) return null;
 
   return (
     <div className="icon-list">
-      {list.map((item, index) => {
+      {list.filter(item => item.name.toLowerCase().includes(searchStr.toLowerCase())).map((item, index) => {
         return (
           <div key={index} className="icon-list__item" title={item.name}>
             {item.icon}
