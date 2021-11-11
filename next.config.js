560ce870266a8bf8e833fc82b02c2fdc0c357ba2
Anton Shlikhta <shlikhta.anton@gmail.com>
2021-11-11 15:41:57 +0200

    update icons

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const isDev = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  swcMinify: true,
   reactStrictMode: true,
   basePath: isDev ? "" : process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: isDev ? "" : process.env.NEXT_PUBLIC_BASE_PATH,
