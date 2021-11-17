0d7e743ab189ab0a8bb21fc9a34d8e3a83a502dc
shlikhta <65901859+shlikhta@users.noreply.github.com>
2021-11-17 07:23:22 +0000

    Deploying to gh-pages from @ shlikhta/next-icons@b2d985be929c2922b298e09e95ffc9b59cc7fce2 🚀

--- _next/static/1wTzyRkaqKjtyxKKIx2Iz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/897-545f011f298ee1a6.js","static/chunks/pages/index-aac2630beb0a672d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/XBrCIseUTSSmeS40Vvdgd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/254-145b478675b5aa37.js","static/chunks/pages/index-ad32f254fbbb1758.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
