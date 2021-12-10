936c2fd1e34899eaf6ca14ad78a2078c1d33eec6
shlikhta <65901859+shlikhta@users.noreply.github.com>
2021-12-10 11:37:28 +0000

    Deploying to gh-pages from @ shlikhta/next-icons@f604de980f19a9d9911831d0c32658b9978a3b23 🚀

---
+++ _next/static/rCXkXaI4Cm0mKmKSGGAvI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/652-c12b1d7b658a8a9b.js","static/chunks/pages/index-57026e4b60b540fe.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
